746e7ff29eca30df0b76f22129dd0c47e38baebb
BrunQuentin <32831076+BrunQuentin@users.noreply.github.com>
2018-11-28 13:37:55 +0100

    creation page addShopping + modif dans home.html et home.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,31 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+// Pour utiliser notre Firebase sur notre appli
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+
 import { MyApp } from './app.component';
+import { FB2Config } from './credentials';
+import { ShoppingListService } from '../services/shopping-list/shopping-list.service';
+
+// pages de mon application 
 //import { HomePage } from '../pages/home/home';
+  
+
 
 @NgModule({
   declarations: [
     MyApp,
-  //  HomePage
+    //HomePage
+    
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+
+    AngularFireModule.initializeApp(FB2Config),
+    AngularFireDatabaseModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,6 +38,8 @@ import { MyApp } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
+    //ajout du service utiliser dans l'appli
+    ShoppingListService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
 
+import { ShoppingListService } from '../../services/shopping-list/shopping-list.service';
+import { Observable } from 'rxjs/Observable';
+import { Item } from '../../models/item/item.model';
+
 @IonicPage()
 @Component({
   selector: 'page-home',
@@ -8,8 +12,21 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+//creation d'un observable
+  shoppingList$: Observable<Item[]>;
 
-  }
+  constructor(public navCtrl: NavController,
+    private shopping: ShoppingListService) 
+    {
+      this.shoppingList$ = this.shopping
+      .getShoppingList() // notre liste dans firebase2
+      .snapshotChanges() // clé et valeurs des article
+      .map(changes => {
+          return changes.map(c => ({
+            key: c.payload.key,...c.payload.val(),
+          }));
+        });
+    }
+
 
 }
